d3614c039aae3275472d49b6239e24505070e709
Babacar GUEYE <gueyebabacar45@gmail.com>
2023-09-12 15:46:04 +0000

    mise a jour

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-
+import React from 'react';
 import './App.css';
-import React, { useEffect, useState } from "react";
+import UserList from './UserList';
 
-const App = () => {
- const [listOfUser, setListOfUser] = useState();// where to store the returned data
- const [error, setError] = useState(null);// where to store the coming errors
- useEffect(() => {
-   function axiosListOfUser() {// the function to fetch data from the api
-     axios.get('/user?ID=1, /user?ID=2, /user?ID=3, /user?ID=4, /user?ID=5, /user?ID=6, /user?ID=7, /user?ID=8, /user?ID=9, /user?ID=10')
-       .then(res => res.json())
-       .then(res => setListOfUser(res))
-       .catch(err => setError(err));
-   }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserList />
+      </header>
+    </div>
+  );
+}
 
-   axiosListOfUser();
- }, []);
- return <div />;
-};
 export default App;
-
